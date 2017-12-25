3d63758740bcb12b249eb758e61ea50d951d4c6d
Negar Mirgati <negar.nbj95@gmail.com>
2017-12-25 00:27:32 -0800

    SYS_IO system call added

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,6 +7,7 @@
 
 static void syscall_handler (struct intr_frame *);
 void check_valid_ptr (const void *vaddr);
+void get_arg (struct intr_frame *f, int *arg, int n);
 
 void
 syscall_init (void) 
@@ -21,22 +22,41 @@ syscall_handler (struct intr_frame *f UNUSED)
   int arg[MAX_ARGS];
   check_valid_ptr((const void*) f->esp);
 
+  switch (* (int *) f->esp){
+
+  	case SYS_IO:{
+
+  		
+  		get_arg(f, arg, 2);
+
+  		/* arg[0] -> int device num, arg[1] -> tid */
+  		add_to_waiting_list(arg[0], arg[1]);
+
+  	}
+
+  }
+
   thread_exit ();
 }
 
 
 void check_valid_ptr (const void *vaddr)
 {
-  if (!is_user_vaddr(vaddr) || vaddr < USER_VADDR_BOTTOM){
-
+  if (!is_user_vaddr(vaddr) || vaddr < USER_VADDR_BOTTOM)
+    {
       exit(ERROR);
     }
+}
 
-    switch (* (int *) f->esp){
 
-    	case SYS_IO : {
-
-    		/**************** Implement system call ******************/
-    	}
+void get_arg (struct intr_frame *f, int *arg, int n)
+{
+  int i;
+  int *ptr;
+  for (i = 0; i < n; i++)
+    {
+      ptr = (int *) f->esp + i + 1;
+      check_valid_ptr((const void *) ptr);
+      arg[i] = *ptr;
     }
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,8 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "devices/my_device.h"
+
 #define ERROR -1
 
 void syscall_init (void);
