6fe06b20bdd31f16dcfef2844f4734ec13771d0f
Negar Mirgati <negar.nbj95@gmail.com>
2018-01-08 00:13:59 -0800

    inform_io edited

--- src/examples/my_device_test.c
+++ src/examples/my_device_test.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <syscall.h>
+#include "threads/thread.h"
 
 int main(){
+
+	struct thread *cur = thread_current();
+	int tid = cur -> tid;
+
+
 	
-	inform_io(1);
+	inform_io(1, tid);
 }
